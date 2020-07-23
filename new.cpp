20afbe73074a638fcf9fc368dd8fe51d0698dfd1
Vikrant Sharma <iamvikrant1@gmail.com>
2020-07-23 20:49:58 +0530

    First beta

---
+++ new.cpp
@@ -0,0 +1,73 @@
+#include <stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<n;i++)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define MAXV(v) *max_element(v.begin(),v.end())
+#define MINV(v) *min_element(v.begin(),v.end())
+#define max3(a, b, c) max(a, max(b, c))
+#define min3(a, b, c) min(a, min(b, c))
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define INT_MAX 2147483647
+#define pow2(x) ((x)*(x))
+#define mod(x, m) ((((x) % (m)) + (m)) % (m))
+
+typedef pair<int,int>	pii;
+typedef pair<ll,ll>	    pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+void solve()
+{
+    cout<<"Hiiii";
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    si(t);
+    while(t--)
+        solve();
+
+    return 0;
+}
+/*
+ Time complexity and algorithms
+    Iterators:-
+                begin() , rbegin()
+                end() , rend()
+
+    Capacity:-
+                size() , resize(), empty(), shrink_to_fit()
+
+    Element Access:-
+                [], at() , front() , back()
+
+    Modifiers:-
+                assign(), push_back(), push_back(), pop_back(), pop_front()
+                insert(), erase(), swap(), clear(), emplace(), emplace_front(), emplace_back()
+    Functions:-
+                swap();
+
+ * */
